b5640abb24cfb528454034c19f3f197f3aa5e038
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-05 13:41:13 -0300

    px

--- resources/views/admin/multimedia.blade.php
+++ resources/views/admin/multimedia.blade.php
@@ -13,7 +13,7 @@
                        
                     <h1 class="text-center text-2xl font-bold my-4">Fotos y Videos</h1>  
                     <div class="p-2 md:hidden">
-                        <iframe width="100%" src="https://www.youtube.com/embed/hNvh1yorQiU?si=Avfzhm9snNGOavuD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <iframe width="100%" height="350px" src="https://www.youtube.com/embed/hNvh1yorQiU?si=Avfzhm9snNGOavuD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div> 
                     <div class="grid grid-cols-3 md:hidden justify-center">
                         
